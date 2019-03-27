4e7b6c37db145de166f0ba3ae87c3069c1933403
Aviral Jain <jain.aviral@siemens.com>
2019-03-27 11:59:23 +0530

    properties for eureka server

--- ConfigApplication/src/main/java/com/springcloud/baeldung/ConfigApplication/ConfigApplication.java
+++ ConfigApplication/src/main/java/com/springcloud/baeldung/ConfigApplication/ConfigApplication.java
@@ -2,7 +2,9 @@ package com.springcloud.baeldung.ConfigApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigApplication {
 
